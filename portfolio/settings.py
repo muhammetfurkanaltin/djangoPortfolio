2ea4fbfeafcb8f9d53bb8f78d36d5a5eb73544b3
Muhammet Furkan ALTIN <m.furkangold@gmail.com>
2026-05-26 19:20:35 +0300

    railway deneme 3

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -7,10 +7,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-8pyj8d^tkw!9cr
 
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get(
-    'DJANGO_ALLOWED_HOSTS',
-    'localhost,127.0.0.1'
-).split(',')
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'pages',
