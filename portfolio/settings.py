c99c2e1190ded5559feba7d0dfd04c3028b0c954
Muhammet Furkan ALTIN <m.furkangold@gmail.com>
2026-05-26 19:43:59 +0300

    railway db deneme 7

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -50,9 +50,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'portfolio.wsgi.application'
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+    "default": dj_database_url.parse(
+        DATABASE_URL,
         conn_max_age=600,
         ssl_require=True
     )
